af72f35987db96af28336c53b937ed7c08d4286f
Leticia Cibele <leticiacibele.cl@gmail.com>
2024-10-04 12:14:27 -0300

    add arquivo categoria.html
    Foi criado os arquivos categorias.html e categorias.css

--- assets/js/login_cadastro.js
+++ assets/js/login_cadastro.js
@@ -1,20 +1,22 @@
-const formularioLogin = document.getElementById('formulario-login');
-const formularioCadastro = document.getElementById('formulario-cadastro');
+const btnPraCadastrar = document.getElementById('btn-para-cadastrar');
 const btnCadastrar = document.getElementById('btn-cadastrar');
-const btnEntrar = document.getElementById('btn-entrar');
+const btnLogar = document.getElementById('btn-logar');
 
-/* Evento de click para mudar entra a seção de login e cadastro */
-btnCadastrar.addEventListener('click', () => {
-    formularioLogin.style.display = 'none';
-    formularioCadastro.style.display = 'block';
+/* Evento de click para alternar entre login e cadastro */
+btnPraCadastrar.addEventListener('click', () => {
+    const secaoLogin = document.getElementById('secao-login');
+    const secaoCadastro = document.getElementById('secao-cadastro');
+
+    secaoLogin.style.display = 'none';
+    secaoCadastro.style.display = 'block';
 });
 
 /* Evento de click para quando o usuário digitar seu login e for correto ir para página principal (index) */
-btnEntrar.addEventListener('click', (event) => {
+btnLogar.addEventListener('click', (event) => {
     event.preventDefault();
 
-    const nome = document.getElementById('nome').value;
-    const senha = document.getElementById('senha').value;
+    const nome = document.getElementById('username-login').value;
+    const senha = document.getElementById('password-login').value;
     const msgErro = document.getElementById('msgErro');
 
     const validarNome = "leticia";
